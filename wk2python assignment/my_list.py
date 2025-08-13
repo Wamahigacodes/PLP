178ccf964486a9c106bf979c92797b49699983c3
Wamahigacodes <wamahiganganga@gmail.com>
2025-08-13 11:23:09 +0300

    Add files via upload

---
+++ wk2python assignment/my_list.py
@@ -0,0 +1,26 @@
+
+my_list = []
+
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+
+my_list.insert(1, 15)  # Index 1 is the second position (0-based index)
+
+
+my_list.extend([50, 60, 70])
+
+
+my_list.pop()  
+
+
+my_list.sort()
+
+
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
+
+
